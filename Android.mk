4f222d83c4c1f456b21ce6bb6728f69f3a8f4368
Daniel Micay <danielmicay@gmail.com>
2017-10-12 13:09:24 -0400

    v1.2

--- Android.mk
+++ Android.mk
@@ -5,6 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := privacy-friendly-netmonitor
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
-LOCAL_SRC_FILES := prebuilt/app-release.apk
+LOCAL_SRC_FILES := prebuilt/app-release-unsigned.apk
 
 include $(BUILD_PREBUILT)
